b9a1c9a6d11fb1d91ded8a20eabf7529ac063720
Luan <luanmvcosta@gmail.com>
2025-06-17 20:49:29 -0300

    Finalizando desafio Iphone pela plataforma DIO, bootcamp SANTANDER

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,26 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Iphone iphone = new Iphone();
 
+        //Testando reprodutor musical:
+        iphone.selecionarMusica("A noite chega (G.A)");
+        iphone.tocar();
+        iphone.pausar();
+
+        System.out.println("<---------------------------------------------->");
+
+        //Testando aparelho telefonico:
+        iphone.ligar("(81) 99840-9867");
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+        System.out.println("<---------------------------------------------->");
+
+        //Testando navegador de internet
+        iphone.exibirPagina("https://github.com/luanmvcosta0");
+        iphone.adicionandoNovaAba();
+        iphone.atualizarPagina();
 
     }
 }
